cf06ef38371f42659fe24fd27d4ae506a11c85d0
VikramBindal <vikrambindal@gmail.com>
2012-10-14 21:29:55 +0530

    First commit for the Android Yamba Project

---
+++ Yamba/src/vikram/app/yamba/TweetCRUDActivity.java
@@ -0,0 +1,22 @@
+package vikram.app.yamba;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class TweetCRUDActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet_crud);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_tweet_crud, menu);
+        return true;
+    }
+
+    
+}
